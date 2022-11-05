c3cc219dc8eefdffc3ee4d4a5819ea5ad91292d7
BrunaSilva <catce.2021211mtds0593@aluno.ifpi.edu.br>
2022-11-04 21:25:29 -0300

    criando listas

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
 import express, { Express, Request, Response } from "express"
+import { type } from "os"
 
 const server: Express = express()
 
+type Animal = {
+    especie: string;
+    raca: string;
+    idade: number;
+    peso: number;
+}
+
+const listaAnimais : Animal[] = []
+
 server.use(express.json())
 
-server.get('api/v1/animals', (req: Request, res: Response) => {
-    res.json({
-        "especie": "Cachorro",
-        "raca": "Pinscher",
-        "idade": "5 anos",
-        "peso": "10 kg"
-    })
+server.get('/api/v1/animals', (req: Request, res: Response) => {
+    res.json(listaAnimais)
 })
 
-server.post('api/v1/animals', (req: Request, res: Response) => {
-    const dados = req.body
+server.post('/api/v1/animals', (req: Request, res: Response) => {
+    const { especie, raca, idade, peso } = req.body
 
-    console.log(dados.especie)
-    console.log(dados.raca)
-    console.log(dados.idade)
-    console.log(dados.peso)
+    console.log(especie)
+    console.log(raca)
+    console.log(idade)
+    console.log(peso)
 
-    res.json({
-        "especie": dados.especie,
-        "raca": dados.raca,
-        "idade": dados.idade,
-        "peso": dados.peso
-    })
+    const newAnimal: Animal = {
+        especie,
+        raca,
+        idade,
+        peso,
+    }
+
+    listaAnimais.push(newAnimal)
+
+    console.log(newAnimal)
+
+    res.json(listaAnimais)
 })
 
 server.listen(3000, () => {
